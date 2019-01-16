773e887ee8d4ccd3a0d1adda1c57d9858c2e1b32
seoksangsik <seoksangsik@alticast.com>
2019-01-16 18:08:50 +0900

    [vui] 3rd commit

--- src/VUI.java
+++ src/VUI.java
@@ -3,5 +3,6 @@ public class VUI {
     public static void main(String[] args) {
         System.out.println("[vui] 1st commit!");
         System.out.println("[vui] 2nd commit!");
+        System.out.println("[vui] 3rd commit!");
     }
 }
